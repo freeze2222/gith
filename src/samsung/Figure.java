c18b7e39270b4431e59e87536899d032c2b532dc
freeze2222 <opop1094@gmail.com>
2021-11-27 19:30:33 +0300

    -_-

--- src/samsung/Figure.java
+++ src/samsung/Figure.java
@@ -4,6 +4,6 @@ import java.awt.*;
 
 public abstract class Figure {
     public void setColor(int r, int g, int b){
-        Color col = new Color(r,g,b);
+        Color color = new Color(r,g,b);
     }}
 
